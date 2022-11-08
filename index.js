38d146c35990afb1aeb04b67048a2ace0b518578
thiagomundici <thiago.santanna@sensedia.com>
2022-11-07 22:28:56 -0300

    Refactoring: Changing index file to be prepared to use the new objects imports

--- index.js
+++ index.js
@@ -1,64 +1,40 @@
-import * as notifier from "./windowsNotifier.js";
-import { getNowTime } from "./getTime.js";
-import { consoleLogPangare, consoleLogIssues } from "./consoleLoggers.js";
-import { sendIssuesMsgToSlack, sendNOIssuesMsgToSlack } from "./slackMessenger.js";
-import { getIssues } from "./gitLabAPIConsumer.js";
-import express from 'express';
-import cron from 'node-cron';
-import dotenv from 'dotenv/config';
-
-
-var app = express();
-
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
-});
-
-app.get("/", function (req, res) {
-  res.send("<center><h1>Pai ta on!!!!</h1></center>");
-});
-
+import { notifier } from "./src/notifiers";
+import { getNowTime, LOGGER } from "./src/utils";
+import { getIssues } from "./src/consumers/gitlab-api.js";
+import cron from "node-cron";
+import dotenv from "dotenv/config";
 
 setInterval(async function () {
+  const nowTime = getNowTime();
+  const issues = await getIssues();
 
-    var nowTime = getNowTime();
- 
-    var issues = await getIssues();
+  if (issues.length !== 0) {
+    for (const issue of issues) {
+      notifier.notify(issue);
 
-    if (issues.length === 0) 
-    {
-      notifier.notifyNoIssues();
-      consoleLogPangare(nowTime);
-    } 
-    else 
-    {
-      issues.forEach(issue => {
-        notifier.notifyIssues(issue);
-        consoleLogIssues(issue, nowTime);
-      });
+      LOGGER.info(nowTime, issue);
     }
-  ;
-}, 5000); //30min - 1800000 ---------- 15min - 900000
+  } else {
+    notifier.notify();
 
+    LOGGER.info(nowTime);
+  }
+}, 5000); //30min - 1800000 ---------- 15min - 900000
 
 //*/5 * * * *  ------ every 5 minutos
 cron.schedule("56 9,11,13,15,17,21 * * *", async function () {
+  const nowTime = getNowTime();
+  const issues = await getIssues();
 
-    var nowTime = getNowTime();
+  if (issues.length !== 0) {
+    for (const issue of issues) {
+      await notifier.sendSlackAlert(issue);
 
-    var issues = await getIssues();
-
-    if (issues.length === 0) 
-    {
-      sendNOIssuesMsgToSlack();
-      consoleLogPangare(nowTime);
-    } 
-    else 
-    {
-      issues.forEach(issue => {
-        sendIssuesMsgToSlack(issue);
-        consoleLogIssues(issue, nowTime);
-      });
+      LOGGER.info(nowTime, issue);
     }
-  
+  } else {
+    await notifier.sendSlackAlert();
+
+    LOGGER.info(nowTime);
+  }
 });
